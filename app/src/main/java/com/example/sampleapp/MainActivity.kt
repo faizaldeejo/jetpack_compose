376b23236b00450180af18e2134c05f749173dbf
faizal <syedfaizal905@gmail.com>
2024-04-05 17:07:10 +0530

    code for buttons

--- app/src/main/java/com/example/sampleapp/MainActivity.kt
+++ app/src/main/java/com/example/sampleapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.sampleapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,7 +13,12 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,7 +27,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
@@ -127,23 +136,92 @@ fun GreetingPreview(modifier: Modifier = Modifier) {
     showSystemUi = true, device = Devices.PIXEL)
 @Composable
 fun TextHeadingStyle() {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .background(Color.Green)
-    ) {
-        Text(
-            text = "Heading 3",
-            style = MaterialTheme.typography.bodyLarge
-        )
-        Text(
-            text = "Heading 4",
-            style = MaterialTheme.typography.bodyMedium
-        )
-        Text(
-            text = "Heading 5",
-            style = MaterialTheme.typography.bodySmall
-        )
+    Column(modifier = Modifier.fillMaxWidth().fillMaxHeight()){
+        Column(
+            modifier = Modifier
+                .background(Color.Green)
+        ) {
+            Text(
+                text = "Heading 3",
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Text(
+                text = "Heading 4",
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Text(
+                text = "Heading 5",
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
+
+        Column(
+            modifier = Modifier.background(Color.Magenta).fillMaxWidth().padding(20.dp)
+        ){
+            Button(onClick = {
+                //your onclick code here
+            },Modifier.align(Alignment.CenterHorizontally),colors = ButtonDefaults.buttonColors(Color.Red))
+            {
+                Text(text = "Button with gray background",color = Color.White)
+            }
+
+            Button(onClick = {
+                //your onclick code here
+            },Modifier.align(Alignment.CenterHorizontally),colors = ButtonDefaults.buttonColors(Color.DarkGray))
+            {
+                Text(text = "Button with gray background",color = Color.White)
+            }
+
+            Button(onClick = {
+                //your onclick code here
+            },Modifier.align(Alignment.CenterHorizontally))
+            {
+                Text(text = "Click ", color = Color.White)
+                Text(text = "Here", color = Color.Green)
+            }
+
+            Button(onClick = {},Modifier.align(Alignment.CenterHorizontally)) {
+                Image(
+                    painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription ="Cart button icon",
+                    modifier = Modifier.size(40.dp))
+
+                Text(text = "Add to cart",Modifier.padding(start = 10.dp),fontSize = 16.sp,
+                    fontFamily = FontFamily.Monospace,
+                    fontWeight = FontWeight.W800)
+            }
+
+            Button(onClick = {},Modifier.align(Alignment.CenterHorizontally) ,shape = RectangleShape) {
+                Text(text = "Rectangle shape")
+            }
+
+            Button(onClick = {},Modifier.align(Alignment.CenterHorizontally), shape = RoundedCornerShape(10.dp)) {
+                Text(text = "Round corner shape")
+            }
+
+            Button(onClick = {}, shape = CutCornerShape(10), border = BorderStroke(2.dp, Color.Cyan)) {
+                Text(text = "Cut corner shape")
+            }
+
+           /* Button(onClick = {
+                //your onclick code here
+            },elevation =  .elevation(
+                defaultElevation = 10.dp,
+                pressedElevation = 15.dp,
+                disabledElevation = 0.dp
+            )) {
+                Text(text = "Button with elevation")
+            }*/
+
+        }
     }
+
+
+
+
+
+
+
 }
+
+
